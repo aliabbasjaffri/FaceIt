4d45479dfdf0137236ca6ce5f165721807dfc1c7
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2016-05-21 01:29:12 +0500

    Adding a new MVC to storyboard

---
+++ FaceIt/EmotionsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EmotionsViewController.swift
+//  FaceIt
+//
+//  Created by Ali Abbas Jaffri on 21/05/2016.
+//  Copyright © 2016 Ali Abbas Jaffri. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
